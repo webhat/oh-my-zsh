5fbf67257dcdc1148ece57c2921e97486b55e308
Daniël W. Crompton <daniel.crompton@gmail.com>
2018-10-18 15:57:48 +0200

    Added tcpdump

---
+++ plugins/tcpdump/tcpdump.plugin.zsh
@@ -0,0 +1,11 @@
+alias tlo="tcpdump -i lo -s 65535 -w"
+
+for i in 0 1 2 4 5 6 7 8 9 ; do
+  alias te$i="tcpdump -i eth$i -s 65535 -w"
+done
+
+if [ -f "$(which ip)" ] ; then
+  for IFACE in $(ip -o link show|cut -d: -f 2); do 
+    alias t$IFACE="tcpdump -i $IFACE -s 65535 -w"
+  done
+fi
